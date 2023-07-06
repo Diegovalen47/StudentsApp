bf6f2787f6e4be9343864dfa56ff4897a7182864
Diegovalen47 <osoriovalentin5@gmail.com>
2023-07-06 00:57:51 -0500

    WIP Login and Register

--- components/layout/AppHeader.vue
+++ components/layout/AppHeader.vue
@@ -3,12 +3,24 @@ import ThemeSwitch from '@/components/layout/ThemeSwitch.vue';
 </script>
 
 <template>
-  <v-toolbar
-    color="primary"
-  >
-    <v-toolbar-title>StudentsApp</v-toolbar-title>
-    <v-spacer></v-spacer>
-    <ThemeSwitch/>
+  <v-toolbar color="primary">
+    <v-row>
+      <v-col>
+        <v-btn
+          class="ml-3"
+          variant="flat"
+          color="surface"
+        >
+          Ingresar
+        </v-btn>
+      </v-col>
+      <v-col class="d-flex justify-center align-center">
+        <h3>StudentsApp</h3>
+      </v-col>
+      <v-col class="d-flex justify-end">
+        <ThemeSwitch/>
+      </v-col>
+    </v-row>
   </v-toolbar>
 </template>
 
