7b32be012fd21fd5170a4dce3c2427a0f9da0680
Diegovalen47 <osoriovalentin5@gmail.com>
2023-07-08 17:22:38 -0500

    Now a student can get registered with normal data, respective validation and persistence in database

--- pages/index.vue
+++ pages/index.vue
@@ -1,23 +1,4 @@
 <script lang="ts" setup>
-import { useGlobalStore } from '@/store/index'
-import { useSwal } from '@/composables/useSwal'
-
-const globalStore = useGlobalStore()
-const { Alert } = useSwal();
-
-// methods
-function incrementHandler() {
-  globalStore.increment()
-  Alert.fire({
-    title: 'Hello world',
-    toast: true,
-    position: 'top-end',
-    text: 'This is a sweet alert',
-    icon: 'success',
-    confirmButtonText: 'Cool',
-    allowOutsideClick: false,
-  })
-}
 </script>
 
 <template>
@@ -31,5 +12,4 @@ function incrementHandler() {
 </template>
 
 <style lang="scss">
-
 </style>
