a9cebff3395eb42600d3e0127400fa98bb3c3e61
Diegovalen47 <osoriovalentin5@gmail.com>
2023-07-16 22:29:01 -0500

    Remove unecesary try/catch in server/controllers/student.ts

--- server/controllers/student.ts
+++ server/controllers/student.ts
@@ -1,104 +1,75 @@
-import { Prisma } from '@prisma/client'
 import prisma from '@/server/controllers/prisma'
 import { Student } from '@/models/Student'
 
 export async function getStudents(): Promise<Student[]> {
-  try {
-    const students = await prisma.student.findMany({
-      select: {
-        studentId: true,
-        userName: true,
-        name: true,
-        lastName: true,
-        email: true,
-        password: true,
-      },
-    })
-    return students
-  } catch (error) {
-    if (error instanceof Prisma.PrismaClientKnownRequestError) {
-      console.log(error.message)
-    }
-    throw error
-  }
+  const students = await prisma.student.findMany({
+    select: {
+      studentId: true,
+      userName: true,
+      name: true,
+      lastName: true,
+      email: true,
+      password: true,
+    },
+  })
+  return students
 }
 
 export async function getStudentById(
   studentId: number
 ): Promise<Student | null> {
-  try {
-    const student = await prisma.student.findUnique({
-      where: {
-        studentId,
-      },
-      select: {
-        studentId: true,
-        userName: true,
-        name: true,
-        lastName: true,
-        email: true,
-        password: true,
-      },
-    })
-    return student
-  } catch (error) {
-    if (error instanceof Prisma.PrismaClientKnownRequestError) {
-      console.log(error.message)
-    }
-    throw error
-  }
+  const student = await prisma.student.findUnique({
+    where: {
+      studentId,
+    },
+    select: {
+      studentId: true,
+      userName: true,
+      name: true,
+      lastName: true,
+      email: true,
+      password: true,
+    },
+  })
+  return student
 }
 
 export async function getStudentByUserName(
   userName: string
 ): Promise<Student | null> {
-  try {
-    const student = await prisma.student.findUnique({
-      where: {
-        userName,
-      },
-      select: {
-        studentId: true,
-        userName: true,
-        name: true,
-        lastName: true,
-        email: true,
-        password: true,
-      },
-    })
-    return student
-  } catch (error) {
-    if (error instanceof Prisma.PrismaClientKnownRequestError) {
-      console.log(error.message)
-    }
-    throw error
-  }
+  const student = await prisma.student.findUnique({
+    where: {
+      userName,
+    },
+    select: {
+      studentId: true,
+      userName: true,
+      name: true,
+      lastName: true,
+      email: true,
+      password: true,
+    },
+  })
+  return student
 }
 
 export async function getStudentByEmail(
   email: string
 ): Promise<Student | null> {
-  try {
-    const student = await prisma.student.findUnique({
-      where: {
-        email,
-      },
-      select: {
-        studentId: true,
-        userName: true,
-        name: true,
-        lastName: true,
-        email: true,
-        password: true,
-      },
-    })
-    return student
-  } catch (error) {
-    if (error instanceof Prisma.PrismaClientKnownRequestError) {
-      console.log(error.message)
-    }
-    throw error
-  }
+  const student = await prisma.student.findUnique({
+    where: {
+      email,
+    },
+    select: {
+      studentId: true,
+      userName: true,
+      name: true,
+      lastName: true,
+      email: true,
+      password: true,
+    },
+  })
+  return student
 }
 
 export async function createStudent(student: Student) {
@@ -115,42 +86,28 @@ export async function createStudent(student: Student) {
 }
 
 export async function updateStudent(student: Student): Promise<Student | null> {
-  try {
-    const result = await prisma.student.update({
-      where: {
-        studentId: student.studentId,
-      },
-      data: {
-        userName: student.userName,
-        name: student.name,
-        lastName: student.lastName,
-        email: student.email,
-        password: student.password,
-      },
-    })
-    return result
-  } catch (error) {
-    if (error instanceof Prisma.PrismaClientKnownRequestError) {
-      console.log(error.message)
-    }
-    throw error
-  }
+  const result = await prisma.student.update({
+    where: {
+      studentId: student.studentId,
+    },
+    data: {
+      userName: student.userName,
+      name: student.name,
+      lastName: student.lastName,
+      email: student.email,
+      password: student.password,
+    },
+  })
+  return result
 }
 
 export async function deleteStudent(
   studentId: number
 ): Promise<Student | null> {
-  try {
-    const result = await prisma.student.delete({
-      where: {
-        studentId,
-      },
-    })
-    return result
-  } catch (error) {
-    if (error instanceof Prisma.PrismaClientKnownRequestError) {
-      console.log(error.message)
-    }
-    throw error
-  }
+  const result = await prisma.student.delete({
+    where: {
+      studentId,
+    },
+  })
+  return result
 }
